7f58ccd973f78d9b1bd797cea4f3a4d515210c36
megra <j@marguerie.org>
2017-03-31 11:15:09 -0700

    Add option to rename the checksum file

--- par2-dir.py
+++ par2-dir.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+import re
 import subprocess
 import sys
 
@@ -14,19 +15,22 @@ def parse_args():
 
     parser.add_argument('op', choices=operations, action='store')
     parser.add_argument('--directory', '-d', help='directory to archive')
+    parser.add_argument('--rename', help='Rename the checksum file using a given prefix', default='checksum')
     parser.add_argument('flags', nargs=argparse.REMAINDER)
 
     # Second level parser because the -d arg get eaten by the flags
     # positional arguments
     parser2 = argparse.ArgumentParser()
     parser2.add_argument('--directory', '-d', help='directory to archive')
+    parser2.add_argument('--rename', help='Rename the checksum file using a given prefix', default='checksum')
     parser2.add_argument('flags', nargs=argparse.REMAINDER)
 
     # Merge.
     args = parser.parse_args()
-    args2 = parser2.parse_args(args.flags)
-    args.flags = args2.flags
+    args2, unknown = parser2.parse_known_args(args.flags)
+    args.flags = args2.flags + unknown
     args.directory = args2.directory
+    args.rename = args2.rename
 
     return args
 
@@ -44,11 +48,27 @@ def main():
                 if not f.endswith('.par2'):
                     files.append(os.path.join(root, f))
 
-    cmd = ['par2', args.op] + args.flags + files
-    print('Running: %s' % ' '.join(cmd))
-    res = subprocess.call(cmd)
-    sys.exit(res)
+    cmd = ['par2', args.op] + args.flags
+    if args.directory:
+        print('Running: %s <files>' % ' '.join(cmd))
+    else:
+        print('Running: %s' % ' '.join(cmd))
 
+    res = subprocess.call(cmd + files)
+    if res:
+        print('Error: return code=%d' % res)
+        sys.exit(res)
+
+    # Rename output files to make them more understandly refer to the
+    # directory. By default par files will take the name of ones of the files.
+    pattern = re.compile(r'^(.+?)(\.vol[0-9]+\+[0-9]+)?\.par2$')
+    for item in os.listdir(args.directory):
+        if os.path.isfile(item) and item.endswith('.par2'):
+            m = re.match(pattern, item)
+            if m:
+                suffix = m.group(2) if m.group(2) else ''
+                new_name = '%s%s.par2' % (args.rename, suffix)
+                os.rename(item, new_name)
 
 if __name__ == '__main__':
     main()
